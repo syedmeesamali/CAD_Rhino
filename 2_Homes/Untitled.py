a3759094274bba23129ba4515efd6dfd2b81d794
syedmeesamali <syedmeesamali@yahoo.com>
2026-04-24 23:48:00 +0300

    Great scripting learning - GH + Python for Rhino

--- 2_Homes/Untitled.py
+++ 2_Homes/Untitled.py
@@ -1,7 +1,8 @@
 #! python 3
 import rhinoscriptsyntax as rs
-
 def helloworld():
     rs.MessageBox ("Hello World")
 
-helloworld()
+pnt = rs.CreatePoint(1.0, 2.0, 3.0)
+pnt = rs.CreatePoint(1.0, 2.0) # This creates a point with the Z coordinate set to 0
+rs.AddLine((45,56,32),(56,47,89))
