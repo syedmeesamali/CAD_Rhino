ca910baa15afa9e2e812e85bfe933358e00b44e6
syedmeesamali <syedmeesamali@yahoo.com>
2026-07-07 23:45:07 +0300

    Some more python in Rhino

--- 3_Coursera/week-1/simple_py.py
+++ 3_Coursera/week-1/simple_py.py
@@ -1,9 +1,5 @@
 import rhinoscriptsyntax as rs
 #Code comments with hash
 
-x = rs.GetInteger('Input integer')
-if (x % 2 == 0):
-    print("Your number is even")
-else:
-    print("Odd number")
-print(x * 2)
+box = rs.GetObject('input a box', rs.filter.polysurface)
+print(box)
