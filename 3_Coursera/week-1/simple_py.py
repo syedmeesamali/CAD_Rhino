da9b86ab34fa6e6236432cf441fa08ea55e0f2dd
syedmeesamali <syedmeesamali@yahoo.com>
2026-07-12 23:53:38 +0300

    adding lines in Rhino Python

--- 3_Coursera/week-1/simple_py.py
+++ 3_Coursera/week-1/simple_py.py
@@ -3,3 +3,6 @@ import rhinoscriptsyntax as rs
 
 box = rs.GetObject('input a box', rs.filter.polysurface)
 print(box)
+r = rs.GetReal('Input real number')
+print(r ** 2)
+rs.AddLine((0,0,0), (r,r*2,0))
