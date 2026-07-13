af152820a4c5868a225d212f3747a0054b8c114d
syedmeesamali <syedmeesamali@yahoo.com>
2026-07-13 23:59:13 +0300

    New codes

--- 3_Coursera/week-1/simple_py.py
+++ 3_Coursera/week-1/simple_py.py
@@ -1,8 +1,6 @@
 import rhinoscriptsyntax as rs
-#Code comments with hash
 
-box = rs.GetObject('input a box', rs.filter.polysurface)
-print(box)
 r = rs.GetReal('Input real number')
 print(r ** 2)
 rs.AddLine((0,0,0), (r,r*2,0))
+rs.AddArc((0,0,0), r * 1.5, 50)
