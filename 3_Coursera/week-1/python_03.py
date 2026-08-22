33d714933cb4621b50098b6a07a06de320bf1c01
syedmeesamali <syedmeesamali@yahoo.com>
2026-08-22 05:02:33 +0300

    More rhino script practice

--- 3_Coursera/week-1/python_03.py
+++ 3_Coursera/week-1/python_03.py
@@ -1,3 +1,13 @@
-x = 10
-x = x + 15
+import rhinoscriptsyntax as rs
+x = 5
+n = 5*(x-2)
+crvId = rs.GetObject("select a curve: ", rs.filter.curve)
+endPt = rs.CurveEndPoint(crvId)
+line = rs.AddLine((0,0,0), endPt)
+
+#output
 print(x)
+print(n)
+print(crvId)
+print(endPt)
+print(line)
