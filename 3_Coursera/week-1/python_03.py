11f518bd9e58cce0e1142bca516422ad76d32b56
syedmeesamali <syedmeesamali@yahoo.com>
2026-09-05 23:27:20 +0300

    Points in Rhino Python

--- 3_Coursera/week-1/python_03.py
+++ 3_Coursera/week-1/python_03.py
@@ -1,13 +1,7 @@
 import rhinoscriptsyntax as rs
-x = 5
-n = 5*(x-2)
-crvId = rs.GetObject("select a curve: ", rs.filter.curve)
-endPt = rs.CurveEndPoint(crvId)
-line = rs.AddLine((0,0,0), endPt)
 
-#output
-print(x)
-print(n)
-print(crvId)
-print(endPt)
-print(line)
+ptGUID = rs.GetObject('Select a point', rs.filter.point)
+print(ptGUID)
+
+pt_Coord = rs.PointCoordinates(ptGUID)
+print(pt_Coord)
