8be2d12ed909e759480d0e2872697c075146bcee
syedmeesamali <syedmeesamali@yahoo.com>
2026-09-25 20:57:11 +0300

    Amazing point moves and GUID

--- 3_Coursera/week-1/python_03.py
+++ 3_Coursera/week-1/python_03.py
@@ -1,9 +1,21 @@
 import rhinoscriptsyntax as rs
+#Points in the rhino space
 
-ptGUID = rs.GetObject('Select a point', rs.filter.point)
-print(ptGUID)
+#ptGUID = rs.GetObject('Select a point', rs.filter.point)
+#print(ptGUID)
 
-pt_Coord = rs.PointCoordinates(ptGUID)
-print(pt_Coord)
+#pt_Coord = rs.PointCoordinates(ptGUID)
+#print(pt_Coord)
 
-rs.AddLine((0,0,0), pt_Coord * 2)
+#Add line with the coordinates
+new_point = (0, 2, 0)
+rs.AddLine(new_point, (5, 10, 0))
+
+startpt = rs.GetObject('select point 1', rs.filter.point)
+endpt = rs.GetObject('select point 2', rs.filter.point)
+
+start1 = rs.PointCoordinates(startpt)
+end1 = rs.PointCoordinates(endpt)
+
+objRect = rs.GetObject('select object', rs.filter.curve)
+rs.MoveObject(objRect, end1 - start1)
